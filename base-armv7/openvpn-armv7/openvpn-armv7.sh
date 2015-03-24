93fc37ffe792fef07abfb04b736ad3cfd3d1023d
oldstreams <oldstreams@gmail.com>
2015-03-24 10:10:14 +0000

    update openvpn

--- base-armv7/openvpn-armv7/openvpn-armv7.sh
+++ base-armv7/openvpn-armv7/openvpn-armv7.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -i -t --privileged $* "greatfox/openvpn-armv7"
+docker run -i -t -p 1194:1194/udp --privileged $* "greatfox/openvpn-armv7"
 
--- ssh/openvpn/openvpn.sh
+++ ssh/openvpn/openvpn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -i -t -p 1194:1194 --privileged $* "greatfox/openvpn"
+docker run -i -t -p 1194:1194/udp --privileged $* "greatfox/openvpn"
 
